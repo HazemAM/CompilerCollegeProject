017e61df91d3f40c4ab04e69bff468f5f51961f8
Hazem AbuMostafa <hbluebrince@gmail.com>
2014-12-07 11:21:31 +0200

    Initial release!
    Hello compilers.
    - Make a Lexical object (then use analyze(...)) to analyze a piece of
    code lexically.
    - DFA transitions are stored (in a specific format, of course) in
    "DFATable.txt".

---
+++ Compiler/CompilerFiles/MainForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Compiler
+{
+    public partial class MainForm : Form
+    {
+        Lexical lexical;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            
+            //Constructing the Lexical object:
+            lexical = new Lexical("..\\..\\DFATable.txt");
+        }
+
+        private void btnRun_Click(object sender, EventArgs e){
+            tableLog.Items.Clear(); //Clear old log.
+            
+            //Analyze input code:
+            lexical.analyze(txtCode.Text);
+            
+            List<string[]> log = lexical.getLog();
+            foreach(string[] row in log)
+                tableLog.Items.Add(new ListViewItem(row));
+            tableLog.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+        }
+
+        private void txtCode_KeyUp(object sender, KeyEventArgs e){
+            if(e.Control && e.KeyCode==Keys.A)
+                (sender as TextBox).SelectAll();
+        }
+    }
+}
