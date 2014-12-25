2d4889778d1d4b243b9e13e88ddfe23ca60f1ce1
Hazem AbuMostafa <hbluebrince@gmail.com>
2014-12-25 20:49:34 +0200

    Syntax Analysis: Getting ready (1).
    - new Token struct, and TokenType enum, to use in next phases.

---
+++ Compiler/CompilerFiles/Token.cs
@@ -0,0 +1,22 @@
+﻿namespace Compiler
+{
+    public struct Token
+    {
+        TokenType type;
+        string value;
+
+        public Token(TokenType type, string value){
+            this.type = type;
+            this.value = value;
+        }
+    }
+
+    public enum TokenType
+    {
+        EOF,
+        ID, Num, NumFraction,
+        If, Then, Else, End, Repeat, Until, Read, Write,
+        Assign, Equal, LessThan, Plus, Minus, Times, LeftParentheses, RightParentheses, SemiColon,
+        AugmentedMinus, AugmentedPlus, Decrement, Increment
+    }
+}
